c087b100eb1fe146ca18ef48e61b1726d2b3604b
Eduart <eduarthajko1992@gmail.com>
2022-11-19 14:43:55 +0100

    OrmProject exception handling

--- ForingKey/Program.cs
+++ ForingKey/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using ForingKey;
+using ForingKey.Models;
+using Microsoft.EntityFrameworkCore;
+public class Program
+{
+
+    static void Main(string[] args)
+    {
+        using (CustomerContext context = new CustomerContext())
+        {
+            var std = new Logs();
+            try
+            {
+                var students = context.Customers
+                  .FromSql($"select * from cusfefhewihfewihfwo")
+                  .ToList();
+            }
+            catch (Exception e)
+            {
+                std.ExceptionName = e.Message;
+                context.Logs.Add(std);
+                context.SaveChanges();
+                Console.WriteLine(e.Message);
+                throw e;
+            }
+        }
+    }
+
+}
+
+    
+
