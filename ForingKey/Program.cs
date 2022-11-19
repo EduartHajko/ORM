d0dbad11c24c1d4205a43be865a2594ce611a8fd
Eduart <eduarthajko1992@gmail.com>
2022-11-19 15:33:48 +0100

    update value in database

--- ForingKey/Program.cs
+++ ForingKey/Program.cs
@@ -12,9 +12,21 @@ public class Program
             var std = new Logs();
             try
             {
-                var students = context.Customers
-                  .FromSql($"select * from cusfefhewihfewihfwo")
-                  .ToList();
+                var updatedvalues = (from a in context.Logs
+                                     where a.ID == 1
+                                     select a).FirstOrDefault();
+                
+                
+                
+                
+                updatedvalues.Name = "Eduart";
+                context.Logs.Update(updatedvalues);
+                context.SaveChanges();
+
+
+                //var students = context.Customers
+                //  .FromSql($"select * from cusfefhewihfewihfwo")
+                //  .ToList();
             }
             catch (Exception e)
             {
